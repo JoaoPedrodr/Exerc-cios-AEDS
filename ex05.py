34bddaf6481195b6f09c5ef13438ef2819b76d07
JoaoPedro <joaopedrodrs11@gmail.com>
2025-03-27 13:20:15 -0300

    5

---
+++ ex05.py
@@ -0,0 +1,12 @@
+a= 10
+b= 20
+c= 3042
+d= 30
+e= 94
+f= 2
+g= 6
+h= 1
+
+total= a+b*c/d*(e+f)*g-1
+
+print(f"O reultado das operações é:{total:.0f}")
---
+++ ex06.py
@@ -0,0 +1,13 @@
+a= 10
+b= 3
+c= 2
+d= 1
+e= 4
+
+total= a % b * a ** c + d - a * e / c
+
+print(f"O reultado das operações é:{total:.0f}")
+
+#O resultado tanto no papel quanto no programa,deram resultados iguais. 
+#Logo é possível perceber que a ordem de precedência é seguida.
+
